213a2c6fc9ae44cba9dec8f32d70c6502e51ef14
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2023-07-04 00:53:29 +0200

    Exercicio do modulo 1
    Ex003
    ex004

---
+++ exercicios/ex03.c
@@ -0,0 +1,22 @@
+#include <stdio.h>  
+
+void main() {
+    char nome[30];
+    int idade;
+    float peso;
+
+    printf("<<< EX003 - Dados >>>\n \n");
+    printf("Qual é o seu nome? ");
+    gets(nome);
+
+    printf("Quantos anos você tem? ");
+    scanf("%i", &idade);
+    printf("Qual é o seu peso? (Kg) ");
+    scanf("%f", &peso);
+    printf("\n \n");
+    printf("-------<<< PROCESSANDO >>>-------\n");
+    printf("Muito prazer, %s. Você tem %i anos e pesa %.3fkg correto?\n", nome, idade, peso);
+    printf("FIM");
+
+
+}
